bde446a7ffa73ce342cd66a5fc28fda4f7b1fa75
ultron005 <begratefuller005@gmail.com>
2022-08-24 15:24:58 +0500

    init

---
+++ src/test/java/uz/shoxrux/my_trelloer/MyTrelloerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.shoxrux.my_trelloer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTrelloerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
